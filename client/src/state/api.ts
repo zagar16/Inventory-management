5af6c3fc22fbf627006d43ec33a67ae859e47e15
zagar16 <zagar.domen1@gmail.com>
2025-02-21 16:46:05 +0100

    dashboard page

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,61 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export interface Product {
+    productId: string;
+    name: string;
+    price: number;
+    rating?: number;
+    stockQuantity: number;
+}
+
+export interface SaleSummary {
+    salesSummaryId: number;
+    totalValue: number;
+    changePercentage?: number;
+    date: string;
+}
+
+export interface PurchaseSummary {
+    purchaseSummaryId: number;
+    totalPurchased: number;
+    changePercentage?: number;
+    date: string;
+}
+
+export interface ExpenseSummary {
+    expenseSummaryId: number;
+    totalExpenses: number;
+    changePercentage?: number;
+    date: string;
+}
+
+export interface ExpenseByCategorySummary {
+    expenseByCategorySummaryId: number;
+    category: string;
+    amount: string;
+    date: string;
+}
+
+export interface DashboardMetrics {
+    popularProducts: Product[]; 
+    saleSummary: SaleSummary[];
+    purchaseSummary: PurchaseSummary[];
+    expenseSummary: ExpenseSummary[];
+    expenseByCategorySummary: ExpenseByCategorySummary[];
+}
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
     reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({}),
+    tagTypes: ["DashboardMetrics"],
+    endpoints: (build) => ({
+        getDashboardMetrics: build.query<DashboardMetrics, void>({
+            query: () => "/dashboard",
+            providesTags: ["DashboardMetrics"],
+        }),
+    }),
 });
 
-export const { } = api;
+export const { 
+    useGetDashboardMetricsQuery
+} = api;
---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import dashboardRoutes from "./routes/dashboardRoutes";
+/* ROUTE IMPORTS */
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("common"));
+
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes);
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
