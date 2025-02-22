ed3e3ad8aa5a2107f73e3178838f7b29a0a89a89
root <root@ip-10-0-1-97.eu-central-1.compute.internal>
2025-02-22 17:54:17 +0000

    Removed node_modules and dist from tracking

--- server/dist/src/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboradController_1 = require("../controllers/dashboradController");
-const router = (0, express_1.Router)();
-router.get("/", dashboradController_1.getDashboardMetrics);
-exports.default = router;
